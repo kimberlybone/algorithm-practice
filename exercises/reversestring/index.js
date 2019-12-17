9aa926400d7f3137ebdc0f29fff63e1080dfe9cf
kimberlybone <k_bone@uncg.edu>
2019-12-17 12:54:20 -0500

    working with reduce

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,12 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+/////////////////// SOLUTION 1 ///////////////////
+
 function reverse(str) {
   const reversedStr = str.split('')
   reversedStr.reverse()
   return reversedStr.join('')
   // return str.split('').reverse().join('')
 }
+
+/////////////////// SOLUTION 2 ///////////////////
 
 // function reverse(str) {
 //   let reversed = ''
@@ -21,5 +25,19 @@ function reverse(str) {
 //   return reversed
 // }
 // new for syntax that eliminates small errors
+
+/////////////////// SOLUTION 3 ///////////////////
+
+function reverse(str) {
+  str.split('').reduce()
+}
+// Reduce is used to take all the different values in an array
+// & condense them all down to 1 singular value
+// So we want to take all the values within the array created
+// & condense them down to a single string
+
+// 💎Reduce💎
+// Takes 2 seperate values
+// The first is an ➡️ function
 
 module.exports = reverse;
