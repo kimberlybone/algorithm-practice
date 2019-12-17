d7c5b8b8c13a4f22959c9772546a3416599e06ee
kimberlybone <k_bone@uncg.edu>
2019-12-17 13:51:20 -0500

    1st solution to palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversedStr = str.split('').reverse().join('')
+  return str === reversedStr ? true : false
+}
 
 module.exports = palindrome;
