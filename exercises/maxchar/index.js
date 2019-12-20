6c8cc09891e47a3dfabbc6b56fc779cae9b16a1b
kimberlybone <k_bone@uncg.edu>
2019-12-20 17:54:11 -0500

    solving maxchars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,8 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {}
+  for( let char of str ){
+    debugger
+    obj[char] = 1
+  }
+}
 
+maxChar('asddddf')
 // Turn the string into an {obj} where each letter is a key
 // and the # of times it appears is the value
 
