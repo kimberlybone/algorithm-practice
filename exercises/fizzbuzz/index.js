f3fb31668bcb13831d1ade158b7edcdd4772c448
kimberlybone <k_bone@uncg.edu>
2019-12-27 19:57:13 -0500

    cleaned code with final solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -17,16 +17,11 @@
 
 function fizzBuzz(n) {
   for( let i = 1; i <= n; i++ ){
-    if( i % 3 === 0 && i % 5 === 0 ){
-      console.log("fizzbuzz")
-    } else if ( i % 3 === 0 ) {
-      console.log("fizz")
-      debugger
-    } else if ( i % 5 === 0 ) {
-      console.log("buzz")
-    } else if (i){
-      console.log(i)
-    }
+    debugger
+    if( i % 3 === 0 && i % 5 === 0 ) console.log("fizzbuzz");
+    else if ( i % 3 === 0 ) console.log("fizz");
+    else if ( i % 5 === 0 ) console.log("buzz");
+    else console.log(i);
   }
 }
 fizzBuzz(20)
@@ -42,7 +37,7 @@ fizzBuzz(20)
 //       debugger
 //     } else if ( i % 5 === 0 ) {
 //       console.log("buzz")
-//     } else if (i){
+//     } else (i){
 //       console.log(i)
 //     }
 //   }
