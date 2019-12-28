28cfb9f09166aa78c81a8a1509c68c2fb06542b6
kimberlybone <k_bone@uncg.edu>
2019-12-27 19:57:44 -0500

    alternate solution finalized

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -17,7 +17,6 @@
 
 function fizzBuzz(n) {
   for( let i = 1; i <= n; i++ ){
-    debugger
     if( i % 3 === 0 && i % 5 === 0 ) console.log("fizzbuzz");
     else if ( i % 3 === 0 ) console.log("fizz");
     else if ( i % 5 === 0 ) console.log("buzz");
